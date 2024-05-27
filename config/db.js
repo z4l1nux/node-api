709a4a1449898c25d4c6aaf50080b06bc4b0523d
Diego Pereira <diegozalinux@gmail.com>
2024-05-27 17:33:12 -0300

    Fix: deprecated connection mongodb

--- config/db.js
+++ config/db.js
@@ -6,10 +6,7 @@ const dbPassword = process.env.DB_PASS;
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(`mongodb+srv://${dbUser}:${dbPassword}@cluster0.hw3ef8f.mongodb.net/mydatabase?retryWrites=true&w=majority&appName=Cluster0`, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+        await mongoose.connect(`mongodb+srv://${dbUser}:${dbPassword}@cluster0.hw3ef8f.mongodb.net/mydatabase?retryWrites=true&w=majority&appName=Cluster0`);
         console.log('Conectou ao banco de dados!');
     } catch (error) {
         console.error('Erro ao conectar ao banco de dados:', error);
